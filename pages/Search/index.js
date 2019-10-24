19992a702c0ed8be77c12dcb3c6519052abf9ed6
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-10-24 17:41:14 +0300

    move file

---
+++ pages/Search/index.js
@@ -0,0 +1,11 @@
+import React from "./node_modules/react";
+import PropTypes from "./node_modules/prop-types";
+import Search from "./Search";
+
+const search = props => {
+  return <Search />;
+};
+
+search.propTypes = {};
+
+export default search;
