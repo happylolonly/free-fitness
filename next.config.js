d1c922be63083026a932b28d23aa74028018ec46
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-10-30 15:56:49 +0300

    change env

--- next.config.js
+++ next.config.js
@@ -6,19 +6,22 @@ const DotenvFlow = require("dotenv-flow-webpack");
 require("dotenv").config();
 
 module.exports = withSass({
+  env: {
+    GTM_KEY: process.env.GTM_KEY
+  },
   webpack: config => {
     config.plugins = config.plugins || [];
 
-    // config.plugins = [
-    //   ...config.plugins,
+    config.plugins = [
+      ...config.plugins,
 
-    //   new Dotenv({
-    //     path: path.join(__dirname, ".env"),
-    //     systemvars: true
-    //   })
-    // ];
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    ];
 
-    config.plugins.push(new DotenvFlow());
+    // config.plugins.push(new DotenvFlow());
 
     return config;
   }
