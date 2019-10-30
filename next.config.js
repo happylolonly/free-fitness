3934a35208ef62f89db500f6cb6cc6369acc3e4e
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-10-30 15:14:20 +0300

    add .env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-// next.config.js
 const withSass = require("@zeit/next-sass");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const DotenvFlow = require("dotenv-flow-webpack");
+
+require("dotenv").config();
+
 module.exports = withSass({
-  /* config options here */
+  webpack: config => {
+    config.plugins = config.plugins || [];
+
+    // config.plugins = [
+    //   ...config.plugins,
+
+    //   new Dotenv({
+    //     path: path.join(__dirname, ".env"),
+    //     systemvars: true
+    //   })
+    // ];
+
+    config.plugins.push(new DotenvFlow());
+
+    return config;
+  }
 });
