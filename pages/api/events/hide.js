58fb6b5a17976c90cc1b7b2a1907c6b37877dc21
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-11-05 11:45:08 +0300

    fix event's hide route

--- pages/api/events/hide.js
+++ pages/api/events/hide.js
@@ -1,6 +1,27 @@
-router.post('/hide', async (req, res) => {
-  console.log('here');
-  debugger;
+// const express = require('express');
+// const router = express.Router();
+const Event = require('../models/event');
+
+// const Event = Even;
+
+let cachedDb = null;
+
+const mongoose = require('mongoose');
+
+async function dbConnect() {
+  if (cachedDb) {
+    return;
+  }
+  try {
+    cachedDb = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    // Even.init();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default async (req, res) => {
+  await dbConnect();
   const { id } = req.body;
   console.log(id);
 
@@ -18,4 +39,4 @@ router.post('/hide', async (req, res) => {
     console.log(error);
     res.status(500).send(error);
   }
-});
+};
