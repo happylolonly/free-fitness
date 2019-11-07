ac6b5130da69cfa91e6827fd0818cc7dad709c0d
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-11-07 11:16:58 +0300

    change theme color

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/images/school-chalao-gym.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#31607B" />
+          <meta name="theme-color" content="#fc805d" />
 
           <meta name="description" content="Все бесплатные фитнес мероприятия в одном месте!" />
           <meta
