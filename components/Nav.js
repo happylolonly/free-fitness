0197514f172d686a022cfaa4449a74e21a331e5f
Valdemar <voldemar136@gmail.com>
2019-11-11 13:48:14 +0300

    transferred code from style.jsx to scss

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Link from "next/link";
-import "./Nav.scss";
+import React from 'react';
+import Link from 'next/link';
+import './Nav.scss';
 
-const links = [{ href: "/search", label: "Мероприятия" }].map(link => {
+const links = [{ href: '/search', label: 'Мероприятия' }].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
 });
@@ -26,11 +26,7 @@ const Nav = () => (
 
     <ul>
       <li>
-        <a
-          href="https://vk.com/im?media=&sel=-129982085"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://vk.com/im?media=&sel=-129982085" target="_blank" rel="noopener noreferrer">
           Написать нам
         </a>
       </li>
@@ -38,31 +34,6 @@ const Nav = () => (
         <Link href="/new">Добавить мероприятие</Link>
       </li>
     </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-        display: flex;
-        padding: 10px 20px;
-        justify-content: space-between;
-        align-items: center;
-      }
-      ul {
-        display: flex;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-    `}</style>
   </nav>
 );
 
