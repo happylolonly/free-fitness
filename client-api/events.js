67aba6a5a2184e97d599b90d19918c2611f169be
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-11-18 12:24:16 +0300

    delete console.log

--- client-api/events.js
+++ client-api/events.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export function getEvents(params) {
-  console.log(`${params.host || ''}/api/events`);
   return axios.get(`${params.host ? 'http://' + params.host : ''}/api/events`, {
     params,
   });
